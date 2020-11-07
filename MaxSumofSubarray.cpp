7b0aa4444fea7f141665ee08b6f46f97ba3ef1fc
Sameeha Mubeen <sameehamubeen81@gmail.com>
2020-11-07 11:55:33 +0530

    Update MaxSumofSubarray.cpp

--- MaxSumofSubarray.cpp
+++ MaxSumofSubarray.cpp
@@ -12,10 +12,32 @@ int maxsum()
     }
     return ans;
 }
+int maxsumind() // print indices of subarray
+{
+    int sum = 0, ans = 0, min_sum = 0, up_ind = 0, lo_ind = 0, min_ind = -1;
+    for(int i = 0; i < n; i++)
+    {
+        sum = sum + a[i];
+        if(ans < sum-min_sum)
+        {
+            ans = sum-min_sum;
+            up_ind = i;
+            lo_ind = min_ind + 1;
+        }
+        if(min_sum > sum)
+        {
+            min_sum = sum;
+            min_ind = i;
+        }
+    }
+    cout<<lo_ind<<" "<<up_ind<<endl;
+}
 int main()
 {
     cin>>n;
     for(int i = 0; i < n; i++)
         cin>>a[i];
+    maxsumind();
     cout<<maxsum()<<"\n";
 }
+//can use this algorithm to find max and min average/arithmetic mean by finding max/min sum dividing it by (r-l+1). r--> right index l --> left index
