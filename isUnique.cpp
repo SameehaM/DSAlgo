403eea78b75f24c8504dade1518dc093d1c46cb7
Sameeha Mubeen <sameehamubeen81@gmail.com>
2020-11-19 00:15:21 +0530

    Create isUnique.cpp

---
+++ isUnique.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long 
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    ll i,n = s.size();
+    sort(s.begin(),s.end());
+    for(i = 0; i < n; i++)                                  // O(nlogn) time complexity.
+    {
+        if(s[i] == s[i+1])
+            break;
+    }
+    if(i == n)
+        cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
+}
